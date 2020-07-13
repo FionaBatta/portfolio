58f4dba28bfa4010ddeb86b34bed17e747d82379
Battaglia Fiona <fiona.battaglia@epitech.eu>
2020-07-13 13:00:59 +0200

    Updates

---
+++ precache-manifest.bfcf8cf6dd12e4d194e00802f219491e.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed6b74ce473bf124f0fb96e7e043862e",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "85069fb54c6d44341150",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "5f87037d35c115fc4a55",
+    "url": "/portfolio/static/css/main.193f03a1.chunk.css"
+  },
+  {
+    "revision": "85069fb54c6d44341150",
+    "url": "/portfolio/static/js/2.a19eb19c.chunk.js"
+  },
+  {
+    "revision": "66aa98ee2c10d8e657138187f5592202",
+    "url": "/portfolio/static/js/2.a19eb19c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f87037d35c115fc4a55",
+    "url": "/portfolio/static/js/main.07829ec1.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "96fdbad97be96bea5a107fdcfd38fdab",
+    "url": "/portfolio/static/media/CV_Battaglia_Fiona.96fdbad9.pdf"
+  },
+  {
+    "revision": "b28e1b712a4ee50920e114e27ed605ac",
+    "url": "/portfolio/static/media/back.b28e1b71.png"
+  },
+  {
+    "revision": "b3c74b842e90c443434ff6dfd1edfc6d",
+    "url": "/portfolio/static/media/back_cont.b3c74b84.png"
+  },
+  {
+    "revision": "ef10ddb16f4720d47308ed727b33c46d",
+    "url": "/portfolio/static/media/fond.ef10ddb1.png"
+  },
+  {
+    "revision": "4d80f94a5e5a4bff7a5b55c014054354",
+    "url": "/portfolio/static/media/linkedin.4d80f94a.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "1f25109576dfd93e55c88a6bd512d0f0",
+    "url": "/portfolio/static/media/med.1f251095.png"
+  },
+  {
+    "revision": "e8124191ffad486b4e948e70002102a1",
+    "url": "/portfolio/static/media/meetic.e8124191.png"
+  },
+  {
+    "revision": "17b4819d02b2af84bf3abf1c84c4d753",
+    "url": "/portfolio/static/media/puissance.17b4819d.png"
+  }
+]);
